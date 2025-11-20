dd92bd0c3c9ef8ce01c7aba2b5fb193b768af298
ravii-bharathi <ravibharathi525@gmail.com>
2025-11-20 15:32:47 +0530

    Revert "workflow trigger"
    This reverts commit bfca71563f972b08e579e2d04cbac4544e17339a.

--- examples/systemcalls/systemcalls.h
+++ examples/systemcalls/systemcalls.h
@@ -1,10 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdarg.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/wait.h>
 
 bool do_system(const char *command);
 
